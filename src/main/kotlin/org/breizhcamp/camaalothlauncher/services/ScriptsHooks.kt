4b35ed8cf6a610be8dc29221b7879ff1fcdb7299
Alexandre Thomazo <alex@thomazo.info>
2019-03-16 23:35:12 +0100

    Don't run scripts hook for preview

--- src/main/kotlin/org/breizhcamp/camaalothlauncher/services/ScriptsHooks.kt
+++ src/main/kotlin/org/breizhcamp/camaalothlauncher/services/ScriptsHooks.kt
@@ -10,11 +10,15 @@ import org.springframework.stereotype.Service
 class ScriptsHooks(private val props: CamaalothProps): NageruHook {
 
     override fun preNageru(preview: Boolean) {
+        if (preview) return
+
         val script = props.preRecordScript ?: return
         ShortCmdRunner("preScript", listOf("/bin/bash", script), true).run()
     }
 
     override fun postNageru(preview: Boolean) {
+        if (preview) return
+
         val script = props.postRecordScript ?: return
         ShortCmdRunner("postScript", listOf("/bin/bash", script), true).run()
     }
